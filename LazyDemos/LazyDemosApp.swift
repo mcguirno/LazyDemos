3d6e4c40350e22c17276063fec19876a62e3d789
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-17 19:43:12 -0500

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by Noah McGuire on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
