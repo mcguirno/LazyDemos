5c92b9874ac8eaf6b91bfb84601edc8e1ca3d33d
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-18 13:22:38 -0500

    Prep for Lazy Stacks in Dungeon Dice

--- LazyDemos/ContentView.swift
+++ LazyDemos/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var columns = [
+        GridItem(.flexible(maximum: 110)),
+        GridItem(.flexible(maximum: 200))
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: columns) {
+                ForEach(0..<1000) { i in
+                    let _ = print("Dang \(i)")
+                    RedAndCyanView()
+                }
+            }
         }
         .padding()
     }
@@ -21,4 +27,15 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct RedAndCyanView: View {
+    var body: some View {
+        ZStack {
+            Color(.red)
+            Rectangle()
+                .fill(.cyan)
+                .frame(width: 100, height: 100)
+        }
+    }
 }
